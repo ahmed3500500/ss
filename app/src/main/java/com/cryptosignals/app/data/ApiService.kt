b9b5b3038f1fcacb5f9b12fbd42e42234a93402f
ahmed3500500 <ahmed3500599@gmail.com>
2026-01-19 22:18:30 -0600

    Update API endpoint to /signals/advanced to match server v2

--- app/src/main/java/com/cryptosignals/app/data/ApiService.kt
+++ app/src/main/java/com/cryptosignals/app/data/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("signals/latest")
+    @GET("signals/advanced")
     suspend fun getSignals(@Query("limit") limit: Int = 20): SignalResponse
 
     companion object {
